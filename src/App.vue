e179124ad2e9fa7d80a8b4bfbdeacf7838cda5e3
Melih Sunbul <melihsunbul68@gmail.com>
2021-06-25 12:34:07 +0300

    code improvements

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 
 <template>
     <div>
-        <div id="input">
+        <div class="input">
             <h3>Set Description</h3>
             <el-input
                 v-model="plainDesc"
                 type="textarea"
-                :rows="2"
-                placeholder="Please input"
+                :rows="10"
+                placeholder="Please enter your app description"
                 autofocus
             />
-            <el-button @click="getInput">
-                <font-awesome-icon :icon="['fas', 'user-secret']"/>Send
+            <el-button :disabled="words.length !== 0" @click="saveInput">
+                <font-awesome-icon :icon="['fas', 'paper-plane']"/> Send Description
             </el-button>
             <p v-if="words.length !== 0">
                 Description sent
             </p>
         </div>
-        <div id="filterWords">
+        <div class="update">
             <h3>Filter Words</h3>
             <el-input v-model.trim="filterWords" type="text"/>
             <p>Please enter words buy putting , between words.</p>
-            <el-button @click="createKeywords">
-                Filter
+            <el-button :disabled="isClickable" @click="createKeywords">
+                Update/Filter
             </el-button>
         </div>
         <div>
@@ -36,14 +36,17 @@
             </el-checkbox-group>
         </div>
 
-        <div id="gram">
+        <div class="gram">
             <div v-for="gramName in grams" :key="gramName">
                 <h4 v-if="words.length !== 0">
                     {{ gramName }}
                 </h4>
-                <el-tag v-for="(word, index) in keywords[gramName]" :key="index" class="tag">
-                    {{ word }}
-                </el-tag>
+                <div v-if="filterArr.length !== 0">
+                    <el-tag v-for="(word, index) in keywords[gramName]" :key="index" class="tag">
+                        {{ word }}
+                    </el-tag>
+                </div>
+                <div v-else/>
             </div>
         </div>
     </div>
@@ -61,10 +64,18 @@
                 grams: [],
                 keywords: [],
                 words: [],
+                isDisabled: true,
             };
+        },
+        computed: {
+            isClickable(){
+                return this.isDisabled || this.words.length === 0;
+            },
+
         },
         methods: {
             createKeywords() {
+
                 this.filterArr = this.filterWords.split(',');
                 this.filterArr = this.filterArr.map(word => word.toLowerCase());
                 this.words = [];
@@ -75,43 +86,47 @@
                 this.words = this.words.filter(word => !this.filterArr.includes(word));
 
 
-                for (let k = 0; k < this.grams.length; k++) {
-                    this.keywords[this.grams[k]] = [];
+                for (let k = 1; k <= 10; k++) {
+                    this.keywords[k] = new Set();
 
                     for (let i = 0; i < this.words.length; i++) {
                         let keyword = '';
 
-                        for (let j = 0; j<this.grams[k]; j++){
+                        for (let j = 0; j<k; j++){
                             if (i + j <= this.words.length - 1) {
                                 keyword += this.words[i + j] + ' ';
                             }
                         }
                         let checkArr = keyword.split(' ');
-                        if (checkArr.length > this.grams[k]) {
-                            this.keywords[this.grams[k]].push(keyword);
+                        if (checkArr.length > k) {
+                            this.keywords[k].add(keyword);
                         }
+
                         keyword = '';
                     }
                 }
-                this.keywords.forEach((keyArr, index) => this.keywords[index] = this.removeDuplicates(keyArr));
 
             },
-            getInput(){
+            saveInput(){
                 const regex = /[!•"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
                 this.plainDesc = this.plainDesc.replace(regex, '');
                 this.createKeywords();
             },
-            removeDuplicates(arr){
-                return [...new Set(arr)];
-            },
+        },
+        beforeUpdate(){
+            this.grams.sort((a,b) => {
+                return a-b;
+            });
         },
         watch: {
-            grams(){
-
-                if (this.words.length !== 0) {
-                    this.createKeywords();
-                }
-
+            plainDesc(){
+                this.isDisabled = false;
+            },
+            filterWords(){
+                this.isDisabled = false;
+            },
+            words(){
+                this.isDisabled = true;
             },
         },
 
@@ -119,13 +134,13 @@
 </script>
 
 <style>
-#input,
-#filterWords {
-text-align: center;
-margin: 20px 20px;
+.input,
+.update {
+  text-align: center;
+  margin: 20px 20px;
 }
-#gram {
-  width: 90vw;
+.gram {
+  width: 100vw;
   overflow: scroll;
   display: flex;
   justify-content: space-around;
