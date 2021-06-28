c6d11160c75d34e59f05d117a5092f11afeb23ed
Melih Sunbul <melihsunbul68@gmail.com>
2021-06-28 14:14:16 +0300

    fix: fontAwesome conf file added

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -2,6 +2,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faPaperPlane);
+library.add(faPaperPlane );
 
 export default FontAwesomeIcon;
